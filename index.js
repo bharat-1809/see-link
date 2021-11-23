65527200929a2d7d505d96294300a2fcb9c7b344
Bharat Sharma <bharat.sharma1809@gmail.com>
2021-11-23 15:45:02 +0530

    add getVideo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import {seelink} from "./lib/seelink.js"
+import { seelink } from "./lib/seelink.js";
 
 (async () => {
-    await seelink("https://github.com/ospfranco/link-preview-js/blob/main/index.ts")
-})()
+  const res = await seelink(
+    "https://www.youtube.com/v/sicTI_2pV6k", "theme", "video");
+  console.log(res);
+})();
